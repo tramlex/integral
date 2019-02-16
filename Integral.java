0c41df34da1b37a5aab2505546dbc58aebe899a1
Тимофеев Алексей <ATimofeev@lanit.ru>
2019-02-16 17:47:48 +0300

    q

--- Integral.java
+++ Integral.java
@@ -3,11 +3,9 @@ public class Integral extends Thread{
 	public static final double NUMBER_OF_THREADS = 10; //число потоков
 	public static final double A = -1; //предел интегрированния
 	public static final double B = 1; //предел интегрированния
-	public static final double DELTA = 0.1; // минимальная разница между значениями
+	public static final double DELTA = 0.01; // минимальная разница между значениями
 	
 	private boolean ok = false ;
-	private double preSum = 0 ;
-	private double postSum = 10;
     private double sum = 0; // Результат суммы в каждом отрезке
     private static double step = (B - A)/NUMBER_OF_THREADS/NUMBER_OF_THREADS; // минимальный шаг
     private double number; //шаг внутри отрезка
@@ -29,20 +27,20 @@ public class Integral extends Thread{
 		{	
 			this.number = A + NumberOfSteps + this.step * i ;
 			double result = Math.pow(this.number , 2);
-			double result2 = Math.pow(this.number+this.step , 2);
-			System.out.println("Шаг= " + this.number + " Результат= " + result + " Delta= " + Math.abs(result2-result));
-			
+			double result2 = Math.pow(this.number + this.step , 2);
+			//System.out.println("Шаг= " + this.number + " Результат= " + result + " Delta= " + Math.abs(result2-result));
+			this.ok = true ;
 			if(Math.abs(result2-result)>DELTA){
 			this.ok = false ;
 			}
 				
-			
+			this.sum = this.sum + result;
 		}
 		turn = turn + 1;
 		this.step = (B - A)/NUMBER_OF_THREADS/turn;
 		}
-		this.step = this.step * turn ;
-		//this.sum = this.sum * this.step;
+		//this.step = this.step * turn ;
+		this.sum = this.sum * this.step;
 		this.NumberOfSteps = this.NumberOfSteps + A;
 		System.out.println("Сумма = " + this.sum + " Начало отрезка = " + this.NumberOfSteps + " Шаг = " + this.step + " Ходы FOR = " + turn);
     }
