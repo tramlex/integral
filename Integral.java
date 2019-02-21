15a47a0bda4f4e25642b74cf20100540d99c7501
Тимофеев Алексей <ATimofeev@lanit.ru>
2019-02-21 08:49:42 +0300

    q

--- Integral.java
+++ Integral.java
@@ -5,12 +5,14 @@ public class Integral extends Thread{
 	public static final double B = 1; //предел интегрированния
 	public static final double DELTA = 0.0001; // минимальная разница между значениями
 	
-	private boolean nextThread = false ; // Условие выхода из цикла
+	//private boolean nextThread = false ; // Условие выхода из цикла
     private double sum = 0; // Результат суммы в каждом отрезке
-    private static double step = (B - A)/NUMBER_OF_THREADS/NUMBER_OF_THREADS; // минимальный шаг
+    //private static double step = (B - A)/NUMBER_OF_THREADS/NUMBER_OF_THREADS; // минимальный шаг
     private double number; //шаг внутри отрезка
 	private double numberOfSteps=0; // число сделанных шагов
 	public static double finish ;
+	private final Object lock = new Object();
+
 
     public Integral(double numberOfSteps) {
 		this.numberOfSteps = numberOfSteps;	//передача значения в поток
@@ -20,28 +22,38 @@ public class Integral extends Thread{
 		System.out.println(">>>>>" + this.finish + "<<<<<"); //вывод конечного результата 
     }
 	
+	public double myMethod(double sum) {
+		synchronized (lock) {
+		return this.finish = this.finish + this.sum;
+		}
+	}
+	
 	@Override
     public void run() {
+		
+		boolean nextThread = false ;
+		double step = (B - A)/NUMBER_OF_THREADS/NUMBER_OF_THREADS;
 		double turn = NUMBER_OF_THREADS; // Кол-во отрезков , на которые разбивается поток
-		while(this.nextThread == false){//выполняется пока условие DELTA не соблюдается 
+		
+		while(nextThread == false){//выполняется пока условие DELTA не соблюдается 
 		this.sum=0;
 		for(int i = 0; i < turn; i++){	
-			this.number = A + numberOfSteps + this.step * i ;
+			this.number = A + numberOfSteps + step * i ;
 			double result = Math.pow(this.number ,2); //здесь задается интегральная функция 
-			double result2 = Math.pow(this.number + this.step ,2);
-			this.nextThread = true ;
+			double result2 = Math.pow(this.number + step ,2);
+			nextThread = true ;
 			if(Math.abs(result2-result)>DELTA){ //проверка условия DELTA
-			this.nextThread = false ;
+			nextThread = false ;
 			}
 		this.sum = this.sum + result;
 		}
 		turn = turn + 1;
-		this.step = (B - A)/NUMBER_OF_THREADS/turn;
+		step = (B - A)/NUMBER_OF_THREADS/turn;
 		}
-		this.sum = this.sum * this.step;
+		this.sum = this.sum * step;
 		this.numberOfSteps = this.numberOfSteps + A;
 		System.out.println("Сумма = " + this.sum + " Ходы FOR = " + turn);
-		this.finish = this.finish + this.sum;
+	    myMethod(this.sum);
     }
 	
 	
